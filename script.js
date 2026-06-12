4d2feba4a637395f346283fd158a97ac4c33d7c9
unknown <udohmichelle3@gmail.com>
2026-06-12 14:44:51 -0700

    separate exercises into individual files

--- script.js
+++ script.js
@@ -1,19 +1,2 @@
 
-//  FUNDAMENTALS EXCERCISE ON JAVASCRIPT.
-
-// CONDITIONALS:
-
-// A SIMPLE AGE CHECKER THAT TELLS IF SOMEONE CAN VOTE.
-
- let age = 18;
-
- if (age > 18){
-    console.log("you can vote!")
- }
- else if(age === 18){
-     console.log("you just turned 18, you can vote")
- }
- else{
-    console.log("sorry you can't vote till you turn 18")
- }
 
