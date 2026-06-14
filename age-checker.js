7ab7b37273635e62c17b2a15f9b2dcf13eafc829
unknown <udohmichelle3@gmail.com>
2026-06-14 04:11:36 -0700

    restore grade classifier and age checker exercise

---
+++ age-checker.js
@@ -0,0 +1,11 @@
+let age = 18;
+
+if(age >18){
+    console.log("you can vote")
+}
+else if(age === 18){
+    console.log("you just turned 18, you can vote")
+}
+else{
+    console.log("sorry you can't vote until you turn 18")
+}
