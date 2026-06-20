302f36181fd001a7300f193a55cbb1748c37deee
unknown <udohmichelle3@gmail.com>
2026-06-19 17:50:30 -0700

    password checker

---
+++ password checker.js
@@ -0,0 +1,25 @@
+// let password ="hello123";
+// console.log(typeof password)
+
+// console.log(password.includes("3"))
+let password1 = "michel";
+let password2 = "michelle"
+let password3 = "michelle@123";
+
+function checkPasswordStrength(password){
+    if(password.length < 6){
+        return "weak"; 
+    }
+    else if(password.length >= 6  && !/\d/.test(password)){
+        return "medium";
+    }
+    else if(password.length >= 8 && /\d/.test(password) && /[!@#$%^&*]/.test(password)){
+        return "strong"; 
+    }
+
+}
+console.log(checkPasswordStrength("michel"))        
+console.log(checkPasswordStrength("michelle"))     
+console.log(checkPasswordStrength("michelle@123"))  
+
+
