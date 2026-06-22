3bce2fc2ec966dcddfdbce643178606b8e640a6b
unknown <udohmichelle3@gmail.com>
2026-06-22 08:30:05 -0700

    student grade book with average, highest & lowest scorer

---
+++ gradebook.js
@@ -0,0 +1,33 @@
+let students = [
+    {name: "mitchelle", score: 65},
+    {name: "chisom", score: 70},
+    {name: "adaora", score: 40},
+    {name: "chidi", score: 40},
+]; 
+let total = 0;
+
+for(let i = 0; i < students.length; i++){
+    total = total + students[i].score;
+}
+
+let average = total / students.length;
+console.log(average) 
+
+let highest = students[0].score;
+
+for(let i = 1; i < students.length; i++){
+    if(students[i].score > highest){
+        highest = students[i].score;
+    }
+}
+console.log(`highest score: ${highest}`);
+
+
+let lowest = students[0].score;
+
+for(let i = 1; i < students.length; i++){
+    if(students[i].score < lowest){
+        lowest = students[i].score;
+    }
+}
+console.log(`lowest score: ${lowest}`); 
